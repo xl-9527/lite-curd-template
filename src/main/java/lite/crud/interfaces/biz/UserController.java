d1ae26ac9f09903025774e4e91c7d7163263f2f5
XingGuangLei <XGL@estone.com>
2024-07-26 14:58:31 +0800

    feat：Take a break for a while, wait for your mood to calm down and continue to improve your business

---
+++ src/main/java/lite/crud/interfaces/biz/UserController.java
@@ -0,0 +1,13 @@
+package lite.crud.interfaces.biz;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xl-9527
+ * @since 2024/7/26
+ **/
+@RestController
+@RequestMapping("/biz/user")
+public class UserController {
+}
---
+++ src/main/java/lite/crud/interfaces/sys/LoginController.java
@@ -0,0 +1,15 @@
+package lite.crud.interfaces.sys;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * user login
+ *
+ * @author xl-9527
+ * @since 2024/7/26
+ **/
+@RestController
+@RequestMapping("/sys/login")
+public class LoginController {
+}
