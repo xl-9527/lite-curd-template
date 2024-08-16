7672868d55a12d98515fa18820e198d11abac1a3
XingGuangLei <XGL@estone.com>
2024-08-16 18:39:40 +0800

    feat(json-opc): 实现与Jackson相关的JSON操作类
    新增一个JSON操作类`JSONOpcJackson`，实现`JSONOpc`接口，提供与Jackson库相关的功能。该实现包括对象转JSON字符串、解析JSON字符串为对象和数组形式，以及指定支持的库为Jackson。
    
    BREAKING CHANGE: `JSONOpcJackson`的引入作为一个新的操作类，可能会导致依赖其他JSON处理类的代码需要重构以适应新的接口和实现。

---
+++ src/main/java/lite/crud/application/util/opc/json/JSONOpcUtil.java
@@ -0,0 +1,41 @@
+package lite.crud.application.util.opc.json;
+
+import java.util.List;
+
+/**
+ * default with jackson
+ *
+ * @author xl-9527
+ * @since 2024/8/16
+ */
+public class JSONOpcUtil implements JSONOpc {
+
+	private final JSONOpc jsonOpc;
+
+	/* JACKSON */
+	public final static JSONOpcUtil DEFAULT = new JSONOpcUtil(JSONOpcEnum.JACKSON);
+
+	public JSONOpcUtil(JSONOpcEnum jsonOpcEnum) {
+		this.jsonOpc = jsonOpcEnum.getJSONOpc();
+	}
+
+	@Override
+	public String toJSONStr(Object obj) {
+		return jsonOpc.toJSONStr(obj);
+	}
+
+	@Override
+	public <T> T parseObject(String jsonStr, Class<T> clazz) {
+		return jsonOpc.parseObject(jsonStr, clazz);
+	}
+
+	@Override
+	public <T> List<T> parseArray(String jsonStr, Class<T> clazz) {
+		return jsonOpc.parseArray(jsonStr, clazz);
+	}
+
+	@Override
+	public JSONOpcEnum supportLib() {
+		return jsonOpc.supportLib();
+	}
+}
