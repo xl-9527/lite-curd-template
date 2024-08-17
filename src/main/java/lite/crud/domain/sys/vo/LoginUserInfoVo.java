80897c5724111613de49104775ef3ccbfbfc5712
XingGuangLei <XGL@estone.com>
2024-08-17 17:45:32 +0800

    refactor(sys,user): 使用record并删除Lombok以提高效率
    将`LoginUserInfoVo`和`UserInfoVo`类转换为record，以利用Java记录的特性减少样板代码并提高性能。此外，删除了`UserInfoVo`类中未使用的Lombok注解，以简化依赖并提高代码的清晰度。

--- src/main/java/lite/crud/domain/sys/vo/LoginUserInfoVo.java
+++ src/main/java/lite/crud/domain/sys/vo/LoginUserInfoVo.java
@@ -1,15 +1,10 @@
 package lite.crud.domain.sys.vo;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.io.Serializable;
 
 /**
  * @author xl-9527
  * @since 2024/7/27
  **/
-//@Getter
-//@Setter
 public record LoginUserInfoVo(Integer id, String username, String password) implements Serializable {
 }
--- src/main/java/lite/crud/domain/user/vo/UserInfoVo.java
+++ src/main/java/lite/crud/domain/user/vo/UserInfoVo.java
@@ -1,8 +1,6 @@
 package lite.crud.domain.user.vo;
 
 import lite.crud.domain.sys.vo.LoginUserInfoVo;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.io.Serializable;
 
@@ -10,17 +8,7 @@ import java.io.Serializable;
  * @author xl-9527
  * @since 2024/8/17
  */
-@Getter
-@Setter
-public class UserInfoVo implements Serializable {
-
-	public static final UserInfoVo EMPTY = new UserInfoVo();
-
-	private Integer id;
-
-	private String username;
-
-	private String password;
+public record UserInfoVo(Integer id, String username, String password) implements Serializable {
 
 	public LoginUserInfoVo toLoginUserInfoVo() {
 		return new LoginUserInfoVo(id, username, password);
