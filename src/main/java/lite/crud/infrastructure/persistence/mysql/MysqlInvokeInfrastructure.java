73665b790cfcfe46edf6c9e23d22ea09f6d710f5
XingGuangLei <XGL@estone.com>
2024-08-27 19:15:46 +0800

    infrastructure层添加新的InvokeInfrastructure接口和实现
    新增一个通用的InvokeInfrastructure接口，以及针对不同基础设施的具体实现，包括本地消息、MQ、MySQL和ES。这提供了基础设施层与应用层之间的抽象，便于未来的扩展和修改。

---
+++ src/main/java/lite/crud/infrastructure/persistence/mysql/MysqlInvokeInfrastructure.java
@@ -0,0 +1,10 @@
+package lite.crud.infrastructure.persistence.mysql;
+
+import lite.crud.infrastructure.InvokeInfrastructure;
+
+/**
+ * @author xl-9527
+ * @since 2024/8/27
+ */
+public interface MysqlInvokeInfrastructure extends InvokeInfrastructure {
+}
